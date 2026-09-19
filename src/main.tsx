1bd48414cb2e25c1bc073c7e3528c9f40c95c525
Ahanaf Mokammel <ahanafmo@gmail.com>
2026-09-19 10:28:32 +0600

    Fix font loading by importing CSS in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './styles/global.css'
 import './styles/typography.css'
+import '../public/fonts/fonts.css'
 import App from './app/App'
 
 createRoot(document.getElementById('root')!).render(
